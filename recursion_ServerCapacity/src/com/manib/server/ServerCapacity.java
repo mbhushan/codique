110907fc44d6a9e06f2ca5ee07ba00ea3a7175a4
manibhushan <manibhushan.cs@gmail.com>
2016-08-15 07:06:39 -0700

    server capacity

--- recursion_ServerCapacity/src/com/manib/server/ServerCapacity.java
+++ recursion_ServerCapacity/src/com/manib/server/ServerCapacity.java
@@ -2,6 +2,18 @@ package com.manib.server;
 
 import java.util.Arrays;
 
+/*
+There are at most eight servers in a data center. Each server has got a capacity/memory limit. 
+There can be at most 8 tasks that need to be scheduled on those servers. Each task requires certain 
+capacity/memory to run, and each server can handle multiple tasks as long as the capacity limit is 
+not hit. Write a program to see if all of the given tasks can be scheduled or not on the servers? 
+
+Example 1: 
+Servers capacity limits: 8, 16, 8, 32   
+Tasks capacity needs: 18, 4, 8, 4, 6, 6, 8, 8   
+For this example, the program should say 'true'. 
+*/
+
 public class ServerCapacity {
 
 	public static void main(String[] args) {
