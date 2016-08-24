63d28c646a7f71da67a5a7b10c520eefd727cf53
manibhushan <manibhushan.cs@gmail.com>
2016-08-23 21:03:00 -0700

    word break - DP solution

--- DP_WordBreakProblem/src/WordBreak.java
+++ DP_WordBreakProblem/src/WordBreak.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -20,11 +21,60 @@ public class WordBreak {
 
 		String [] text = {"thisissent", "weloveprogramming", "insideawesomeprogramming"};
 		
-		//for (int i=0; i<text.length; i++) {
-		for (int i=0; i<1; i++) {
+		for (int i=0; i<text.length; i++) {
+		//for (int i=0; i<1; i++) {
 			System.out.println(wb.wordBreak(text[i]));
+			System.out.println("DP: " + wb.wordBreakDP(text[i]));
 		}
 		
+	}
+	
+	public String wordBreakDP(String text) {
+		if (text == null || text.length() < 1) {
+			return new String(text);
+		}
+		int n = text.length();
+		int [][] dp = new int[n][n];
+		
+		for (int i=0; i<n; i++) {
+			Arrays.fill(dp[i], -1);
+		}
+		
+		for (int len=1; len<=n; len++) {
+			for (int i=0; i<n-len+1; i++) {
+				int j = i + len -1;
+				String buff = text.substring(i, j+1);
+				if (dict.contains(buff)) {
+					dp[i][j] = i;
+					continue;
+				}
+				for (int k=i+1; k<=j; k++) {
+					if (dp[i][k-1] != -1 && dp[k][j] != -1) {
+						dp[i][j] = k;
+						break;
+					}
+				}
+			}
+		}
+		if (dp[0][n-1] != -1) {
+			System.out.println("word break possible!");
+		} else {
+			System.out.println("word break NOT possible!");
+			return null;
+		}
+		int start = 0;
+		int end = dp[0][n-1];
+		List<String> result = new ArrayList<String>();
+		while (start != end) {
+			String buff = text.substring(start, end);
+			result.add(buff);
+			start = end;
+			end = dp[end][n-1];
+			if (start == end) {
+				result.add(text.substring(start, n));
+			}
+		}
+		return result.toString();
 	}
 	
 	public String wordBreak(String text) {
@@ -50,7 +100,7 @@ public class WordBreak {
 		}
 		
 		for (int i=index; i<text.length; i++) {
-			System.out.println(i+"," +index+" tl,"+text.length);
+			//System.out.println(i+"," +index+" tl,"+text.length);
 			char ch = text[i];
 			sb.append(ch);
 			if (dict.contains(sb.toString())) {
