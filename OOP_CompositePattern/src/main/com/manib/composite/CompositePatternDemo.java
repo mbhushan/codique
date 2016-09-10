a3fd6147e598e336b6c22267b785bca3d9687a97
manibhushan <manibhushan.cs@gmail.com>
2016-09-10 11:52:59 -0700

    composite pattern demo

--- OOP_CompositePattern/src/main/com/manib/composite/CompositePatternDemo.java
+++ OOP_CompositePattern/src/main/com/manib/composite/CompositePatternDemo.java
@@ -2,4 +2,32 @@ package main.com.manib.composite;
 
 public class CompositePatternDemo {
 
+	public static void main(String[] args) {
+		Employee CEO = new Employee("Mani", "CEO", 30000);
+
+		Employee headSales = new Employee("Shreya", "Head Sales", 20000);
+
+		Employee headMarketing = new Employee("Arch", "Head Marketing", 20000);
+
+		Employee clerk1 = new Employee("Lily", "Marketing", 10000);
+		Employee clerk2 = new Employee("Rose", "Marketing", 10000);
+
+		Employee salesExecutive1 = new Employee("Rish", "Sales", 10000);
+		Employee salesExecutive2 = new Employee("Soma", "Sales", 10000);
+
+		CEO.addEmployee(headSales);
+		CEO.addEmployee(headMarketing);
+
+		headSales.addEmployee(salesExecutive1);
+		headSales.addEmployee(salesExecutive2);
+
+		headMarketing.addEmployee(clerk1);
+		headMarketing.addEmployee(clerk2);
+
+		CEO.printEmployeeHeirarchy();
+		System.out.println("=======================================================================================");
+		headMarketing.printEmployeeHeirarchy();
+		System.out.println("=======================================================================================");
+		headSales.showEmployees();
+	}
 }
--- OOP_CompositePattern/src/main/com/manib/composite/Employee.java
+++ OOP_CompositePattern/src/main/com/manib/composite/Employee.java
@@ -45,19 +45,49 @@ public class Employee {
 		return sb.toString();
 	}
 	
-	public void printEmployeeHeirarchy(Employee employee) {
-		if (employee == null) {
-			return ;
-		}
+	public void showEmployees() {
+		Employee emp = this;
+		showEmployees(emp);
 		
+	}
+	
+	private void showEmployees(Employee emp) {
+		if (emp == null) {
+			return;
+		}
+		System.out.print(emp + "\t");
+		
+		int size = emp.subordinates.size();
+		for (int i=0; i<size; i++) {
+			Employee e = emp.subordinates.get(i);
+			showEmployees(e);
+		}
+		System.out.println();
+	}
+	
+	public void printEmployeeHeirarchy() {
+		Employee employee = this;
 		
 		Queue<Employee> queue = new LinkedList<Employee>();
 		queue.add(employee);
 		
 		Employee marker = new Employee();
+		queue.add(marker);
 		
 		while (!queue.isEmpty()) {
-			
+			Employee e = queue.remove();
+			if (e.equals(marker)) {
+				System.out.println();
+				if (!queue.isEmpty()) {
+					queue.add(marker);
+				}
+			} else {
+				System.out.print(e + "\t");
+				int size = e.subordinates.size();
+				for (int i=0; i<size; i++) {
+					queue.add(e.subordinates.get(i));
+				}
+			}
 		}
 	}
 	
