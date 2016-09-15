53abc81afa514234f3d4fbf2dcc08aae96f97ead
manibhushan <manibhushan.cs@gmail.com>
2016-09-14 19:24:35 -0700

    begin, rollback and commit support

--- PRJ_SimpleDatabase/src/main/com/db/impl/Command.java
+++ PRJ_SimpleDatabase/src/main/com/db/impl/Command.java
@@ -1,16 +1,24 @@
 package main.com.db.impl;
 
+import java.util.Stack;
+
 public class Command {
 
 	private static final String SET = "SET";
 	private static final String GET = "GET";
 	private static final String UNSET = "UNSET";
 	private static final String NUMEQUALTO = "NUMEQUALTO";
+	private static final String BEGIN = "BEGIN";
+	private static final String ROLLBACK = "ROLLBACK";
+	private static final String COMMIT = "COMMIT";
 
 	private Database<String> db;
+	private Stack<Database<String>> stack;
 
 	public Command() {
-		db = new Database<String>();
+		this.db = new Database<String>();
+		this.stack = new Stack<Database<String>> ();
+		this.stack.push(this.db);
 	}
 
 	public void execute(String cmd) {
@@ -27,24 +35,37 @@ public class Command {
 		case SET:
 			name = tokens[1].trim();
 			value = tokens[2].trim();
-			db.set(name, value);
+			stack.peek().set(name, value);
 			break;
 
 		case GET:
 			name = tokens[1];
-			value = db.get(name);
+			value = stack.peek().get(name);
 			System.out.println(value);
 			break;
 
 		case UNSET:
 			name = tokens[1];
-			db.unset(name);
+			stack.peek().unset(name);
 			break;
 
 		case NUMEQUALTO:
 			value = tokens[1];
-			int count = db.numequalto(value);
+			int count = stack.peek().numequalto(value);
 			System.out.println(count);
+			break;
+			
+		//transaction commands
+		case BEGIN:
+			begin();
+			break;
+			
+		case ROLLBACK:
+			rollback();
+			break;
+			
+		case COMMIT:
+			commit();
 			break;
 			
 		default:
@@ -55,6 +76,29 @@ public class Command {
 	private String[] parse(String input) {
 		input = input.replaceAll(" +", " ");
 		return input.split(" ");
+	}
+	
+	public void begin() {
+		Database<String> db = new Database<String>(this.stack.peek());
+		this.stack.push(db);
+	}
+	
+	public void rollback() {
+		if (this.stack.size() == 1) {
+			System.out.println("NO TRANSACTION");
+		} else {
+			this.stack.pop();
+		}
+	}
+	
+	public void commit() {
+		if (this.stack.size() == 1) {
+			System.out.println("NO TRANSACTION");
+		} else {
+			this.db = new Database<String>(this.stack.peek());
+			this.stack = new Stack<Database<String>>();
+			this.stack.push(this.db);
+		}
 	}
 
 }
