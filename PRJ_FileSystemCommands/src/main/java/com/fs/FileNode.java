407cb2b8e818514da1276e4eea1cd85ab03366df
manibhushan <manibhushan.cs@gmail.com>
2016-09-23 06:47:20 -0700

    file system node class

---
+++ PRJ_FileSystemCommands/src/main/java/com/fs/FileNode.java
@@ -0,0 +1,64 @@
+package main.java.com.fs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FileNode {
+	
+	private String name;
+	private StringBuffer content;
+	private boolean isDirectory;
+	
+	private FileNode parent; //parent directory
+	
+	private Map<String, FileNode> files; //list of files in the current directory.
+	
+	public FileNode(String name) {
+		this.name = name;
+		this.content = null;
+		this.parent = null;
+		this.files = new HashMap<String, FileNode>();
+	}
+	
+	public FileNode getParent() {
+		return this.parent;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setParent(FileNode node) {
+		this.parent = node;
+	}
+	
+	public void isDirectory(boolean flag) {
+		this.isDirectory = flag;
+	}
+	
+	public void addFile(String name, FileNode node) {
+		this.files.put(name, node);
+	}
+	
+	public  Map<String, FileNode> getFiles() {
+		return this.files;
+	}
+	
+	
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append("[");
+		sb.append("file: " + this.name + ", dir: " + this.isDirectory);
+		if (this.files != null && this.files.size() > 0) {
+			sb.append(", ");
+			for (String name: this.files.keySet()) {
+				sb.append(name + ", ");
+			}
+		}
+		sb.append("]");
+		
+		return sb.toString();
+	}
+	
+}
